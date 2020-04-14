5eab7297ab31f3876ec762a1076878632eaf0b83
Dion Kim <20143341@changwon.ac.kr>
2020-04-14 18:01:08 +0900

    Add files via upload

---
+++ UITest/UITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITest
+//
+//  Created by mac on 2020/04/14.
+//  Copyright © 2020 dionkim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
